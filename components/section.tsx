f2de680a57932862e11f0ef1bb0ff51336ec953b
ajagatobby <150749949+ajagatobby@users.noreply.github.com>
2025-06-14 16:52:47 +0100

    added: updated padding

--- components/section.tsx
+++ components/section.tsx
@@ -5,11 +5,11 @@ import { Card } from "./ui";
 const Section = ({ sectionTitle, screenShots }: SectionProps) => {
   return (
     <React.Fragment>
-      <div className="w-full flex items-center justify-between mt-2 lg:px-0 px-2">
+      <div className="w-full flex items-center justify-between mt-2 lg:px-0 px-4">
         <p className="text-lg text-[#333]">{sectionTitle}</p>
       </div>
 
-      <div className="w-full grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2  gap-1 my-2 lg:px-0 px-2">
+      <div className="w-full grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2  gap-1 my-2 lg:px-0 px-4">
         {screenShots.map((screenShot) => (
           <Card
             title={screenShot.title}
