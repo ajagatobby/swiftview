1dcc5fbe9d4f638ae1d1c22fb7562a69ce7d2992
ajagatobby <150749949+ajagatobby@users.noreply.github.com>
2025-06-14 20:48:39 +0100

    added: new screens

--- Utilities/Mock/signin-screen.ts
+++ Utilities/Mock/signin-screen.ts
@@ -6,4 +6,11 @@ export const signInScreenShots = [
     appName: "Hola",
     videoUrl: "/videos/signin-screen-1.mp4",
   },
+  {
+    title: "Sign in screen",
+    image: "pictora.png",
+    link: "https://www.pictora.com/signin",
+    appName: "Pictora",
+    videoUrl: "/videos/signin-screen-2.mp4",
+  },
 ];
--- Utilities/Mock/welcome-screen.ts
+++ Utilities/Mock/welcome-screen.ts
@@ -6,4 +6,11 @@ export const welcomeScreenShots = [
     appName: "Hola",
     videoUrl: "/videos/welcome-screen-1.mp4",
   },
+  {
+    title: "Welcome screen",
+    image: "pal.png",
+    link: "https://www.pal.com/welcome",
+    appName: "Pal",
+    videoUrl: "/videos/welcome-screen-2.mp4",
+  },
 ];
